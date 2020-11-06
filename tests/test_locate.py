1502b87990bba6a0c7119d2995ea759fc62992b7
Geert Barentsen <geert@barentsen.be>
2020-11-06 09:52:54 -0800

    Add test for searching by string

--- tests/test_locate.py
+++ tests/test_locate.py
@@ -11,10 +11,13 @@ from tess_locator import locate
 
 def test_pi_men():
     """Tests `locate()` against `astroquery.mast.Tesscut.get_sectors()`"""
-    crd = SkyCoord(ra=84.291188, dec=-80.46911982, unit='deg')
+    crd = SkyCoord(ra=84.291188, dec=-80.46911982, unit="deg")
     mast_result = Tesscut.get_sectors(crd)
     our_result = locate(crd)
     assert len(mast_result) == len(our_result)
-    our_result_df = our_result.to_pandas()[['sector', 'camera', 'ccd']]
-    mast_result_df = mast_result[['sector', 'camera', 'ccd']].to_pandas()
+    our_result_df = our_result.to_pandas()[["sector", "camera", "ccd"]]
+    mast_result_df = mast_result[["sector", "camera", "ccd"]].to_pandas()
     assert our_result_df.equals(mast_result_df)
+    # Can we search by passing a string?
+    our_result2 = locate("Pi Men")
+    assert our_result == our_result2
