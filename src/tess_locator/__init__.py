01e131fac2cbcadd9a1b37370c5854693b348559
Jorge Martínez-Palomera <jorgemarpa@ug.uchile.cl>
2023-09-28 13:58:03 -0400

    up to v 0.6.0

--- src/tess_locator/__init__.py
+++ src/tess_locator/__init__.py
@@ -2,7 +2,7 @@ import logging
 from pathlib import Path
 import tess_stars2px  # provided by the `tess-point` package
 
-__version__ = "0.6.dev"
+__version__ = "0.6.0"
 
 # Where does this package store its embedded data?
 PACKAGEDIR: Path = Path(__file__).parent.absolute()
